f20f7d254a7ed7b5d8a441c92d8a34a19c83a8de
Niklas Burrichter <niklas.burrichter@stud.h-da.de>
2015-05-04 13:19:16 +0200

    Projectile added

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,22 +3,64 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour {
 
+	//Inspector Variables
+	public GameObject prefabProjectile;
+
+	//Internal state Variables
 	private GameObject launchPoint;
+	private bool aimingMode;
+
+	private GameObject projectile;
+	private Vector3 launchPos;
 
 	void Awake() {
 		Transform launchPointTrans = transform.Find("Launchpoint");
 		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive(false);
+		launchPos = launchPointTrans.position;
 	}
 
     void OnMouseEnter() {
 		//print ("Slingshot:MouseEnter");
-		launchPoint.SetActive (true);
-		//launchPoint.GetComponent<halo> ().SetActive (true);
+		launchPoint.SetActive (true);	
 	}
 
 	void OnMouseExit(){
 		//print ("Slingshot:MouseExit");
 		launchPoint.SetActive (false);
+	}
+
+	void OnMouseDown() {
+		//set the game to aiming mod
+		aimingMode = true;
+
+		//instantiate a projectile at launchpoint
+		projectile = Instantiate (prefabProjectile) as GameObject;
+		projectile.transform.position = launchPos; 
+
+		//Switch off physics for now
+		projectile.GetComponent<Rigidbody> ().isKinematic = true;
+
+	}
+
+	void Update() {
+		if (!aimingMode)
+			return;
+
+		//get mouse pos and convert it to 3D
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = - Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);     
+
+		//Calculate the delta between launch position and mouse position
+		Vector3 mouseDelta = mousePos3D - launchPos;
+
+		//Constrain the delta to the maximum of the sphere collider
+		float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+		mouseDelta = Vector3.ClampMagnitude (mouseDelta,maxMagnitude);
+
+		//set projectile position to new position and fire it
+		projectile.transform.position = launchPos + mouseDelta;
+
 	}
 }
